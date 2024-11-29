6e258ca8b4963bcb0c9679e4bf4f2841cb73e08f
Pap3rClips <hcostarobert@guardiaschool.fr>
2024-11-29 10:12:41 +0100

    contact php first meth

--- contact.php
+++ contact.php
@@ -37,9 +37,7 @@ $localisation = $contact['localisation'];
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
     
-    ?>
     <nav>
         <div class="nav-content">
             <div class="logo">A.S</div>
@@ -55,21 +53,40 @@ $localisation = $contact['localisation'];
         <section class="contact">
             <h1>Contact</h1>
             <div class="contact-content">
-                <form id="contact-form" onsubmit="handleSubmit(event)">
+                <form id="contact-form" method="POST" action="contact.php">
                     <div class="form-group">
                         <label for="name">Nom</label>
-                        <input type="text" id="name" required>
+                        <input type="text" id="name" name='name' required>
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" id="email" required>
+                        <input type="email" id="email" name='email' required>
                     </div>
                     <div class="form-group">
                         <label for="message">Message</label>
-                        <textarea id="message" required></textarea>
+                        <textarea id="message" name='message' required></textarea>
                     </div>
                     <button type="submit">Envoyer</button>
                 </form>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $post_nom = $_POST['name'];
+                    $post_email = $_POST['email'];
+                    $post_message = $_POST['message'];
+
+                    require_once(__DIR__.'\config\mysql.php');
+                    $connexion = mysqli_connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB_NAME);
+
+                    $sql = "INSERT INTO messages (`nom`, `email` , `message`) VALUES ('$post_nom', '$post_email', '$post_message')";
+
+                    if (mysqli_query($connexion, $sql)) {
+                        echo "Nouveau message envoyé";
+                    } else {
+                        echo "Erreur : " . $sql . "<br>" . mysqli_error($connexion);
+                    }
+                } else {
+                }
+                ?>
                 <div class="contact-info">
                     <h2>Informations de contact :</h2>
                 <ul>
@@ -89,3 +106,5 @@ $localisation = $contact['localisation'];
     <script src="js/script.js"></script>
 </body>
 </html>
+
+
